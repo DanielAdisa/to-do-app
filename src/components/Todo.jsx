10b1237ce77a86551df67ffab650c802de0d3d86
Your Name <you@example.com>
2024-09-20 16:22:54 +0100

    asas

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,7 +3,7 @@ import { RiMenuAddFill } from "react-icons/ri";
 import { IoIosAddCircle } from "react-icons/io";
 import { MdOutlinePlaylistAddCheck } from "react-icons/md";
 import TodoItems from "./TodoItems";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Todo = () => {
  
@@ -33,27 +33,42 @@ const Todo = () => {
         })
     }
 
+    const toggle = (id)=>{
+        setTodoList((prevTodos)=>{
+            return prevTodos.map((todo)=>{
+                if(todo.id === id){
+                    return {...todo, isComplete:!todo.isComplete}
+                }
+                return todo;
+            })
+        })
+    }
+
+    useEffect(()=>{
+        console.log(todoList);
+    },[todoList])
+
   return (
-    <div className="flex flex-col w-11/12 max-w-md border bg-gradient-to-br from-stone-500 to-stone-800 border-stone-300 place-self-center p-7 min-h-fit rounded-xl">
+    <div className="flex flex-col w-11/12 max-w-md transition-all border bg-gradient-to-br from-stone-500 to-stone-800 border-stone-300 place-self-center p-7 min-h-fit rounded-xl">
         
         {/* Title */}
         
-        <div className="flex items-center gap-2 mt-7">
+        <div className="flex items-center gap-2 transition-all mt-7">
         <LuListTodo className="text-3xl " />
         <h1 className="text-xl font-semibold ">To-Do List</h1>
         </div>
 
         {/* Input Field */}
-        <div className="flex items-center justify-center text-sm rounded-md my-7 bg-stone-200 text-stone-300">
+        <div className="flex items-center justify-center text-sm transition-all rounded-md my-7 bg-stone-200 text-stone-300">
             <input ref={inputRef} className="flex-1 pl-6 pr-2 bg-transparent border-0 outline-none text-slate-800 h-14 placeholder:text-slate-800" type="text" placeholder="What do you want to get done?"/>
             <button  onClick={add} className="flex items-center p-4 font-semibold border-none rounded-md cursor-pointer text-md bg-stone-900 w-30 h-14">Add Task{" "}<MdOutlinePlaylistAddCheck className="ml-1 text-xl " /></button>
         </div>
         
             {/* To-Do List */}
 
-            <div>
+            <div className="transition-all">
                 {todoList.map((item, index) =>{
-                    return <TodoItems key={index} text={item.text} id={item.id}  isComplete={item.isComplete} deleteTodo={deleteTodo} />
+                    return <TodoItems key={index} text={item.text} id={item.id}  isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle} />
                 })}
                 {/* <TodoItems text={"Well Hello There"} />
                 <TodoItems text={"Eat 4 Eggs"}/> */}
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,16 +1,18 @@
 import { IoCheckbox } from "react-icons/io5";
 import { MdIndeterminateCheckBox } from "react-icons/md"
 
-const TodoItems = ({text, id, isComplete, deleteTodo}) => {
+const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
   return (
-    <div className="flex items-center gap-2 my-3 ">
-        <div className="flex items-center flex-1 cursor-pointer ">
-           <IoCheckbox className="text-2xl w-7"/> 
+    <div className="flex items-center gap-2 my-3 transition-all">
+        <div onClick={()=>{toggle(id)}} className="flex items-center flex-1 transition-all cursor-pointer ">
+          <IoCheckbox className={isComplete? "text-green-700 text-3xl w-7 transition-all antialiased":"text-red-500 transition-all text-2xl w-7"}/>
+           {/* <IoCheckbox className="text-2xl w-7"/>  */}
            <p className=" text-stone-50 ml-4 text-[15px]">{text}</p>
           
 
         </div>
-        <MdIndeterminateCheckBox onClick={()=>{deleteTodo(id)}} className="text-2xl w-7"/>
+        
+        <MdIndeterminateCheckBox  onClick={()=>{deleteTodo(id)}} className="text-2xl w-7"/>
     </div>
   )
 }
