552e22e41bae72f7c171c9873a2454bace190a92
Your Name <you@example.com>
2024-09-20 13:01:32 +0100

    aas

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,38 +3,54 @@ import { RiMenuAddFill } from "react-icons/ri";
 import { IoIosAddCircle } from "react-icons/io";
 import { MdOutlinePlaylistAddCheck } from "react-icons/md";
 import TodoItems from "./TodoItems";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const Todo = () => {
+ 
+    const [todoList,setTodoList] = useState([]);
 
     const inputRef = useRef();
 
     const add = ()=>{
         const inputText = inputRef.current.value.trim();
-        console.log(inputText);
+
+        if (inputText === ""){
+            return null;
+        }
+
+        const newTodo = {
+            id: Date.now(),
+            text: inputText,
+            isComplete: false,
+        }
+        setTodoList((prev)=>[...prev,newTodo]);
+        inputRef.current.value = "";
     }
 
   return (
-    <div className=" bg-gradient-to-br from-stone-500 to-stone-800 border border-stone-300 place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-fit rounded-xl">
+    <div className="flex flex-col w-11/12 max-w-md border bg-gradient-to-br from-stone-500 to-stone-800 border-stone-300 place-self-center p-7 min-h-fit rounded-xl">
         
         {/* Title */}
         
-        <div className=" flex items-center mt-7 gap-2">
-        <LuListTodo className=" text-3xl" />
-        <h1 className=" text-xl font-semibold">To-Do List</h1>
+        <div className="flex items-center gap-2 mt-7">
+        <LuListTodo className="text-3xl " />
+        <h1 className="text-xl font-semibold ">To-Do List</h1>
         </div>
 
         {/* Input Field */}
-        <div className=" text-sm flex items-center my-7 rounded-md bg-stone-200 justify-center text-stone-300">
-            <input ref={inputRef} className=" bg-transparent border-0 outline-none text-slate-800 flex-1 h-14 pl-6 pr-2 placeholder:text-slate-800" type="text" placeholder="What do you want to get done?"/>
-            <button  onClick={add} className="  text-md font-semibold p-4 flex items-center border-none rounded-md bg-stone-900  w-30 h-14 cursor-pointer">Add Task{" "}<MdOutlinePlaylistAddCheck className=" ml-1 text-xl" /></button>
+        <div className="flex items-center justify-center text-sm rounded-md my-7 bg-stone-200 text-stone-300">
+            <input ref={inputRef} className="flex-1 pl-6 pr-2 bg-transparent border-0 outline-none text-slate-800 h-14 placeholder:text-slate-800" type="text" placeholder="What do you want to get done?"/>
+            <button  onClick={add} className="flex items-center p-4 font-semibold border-none rounded-md cursor-pointer text-md bg-stone-900 w-30 h-14">Add Task{" "}<MdOutlinePlaylistAddCheck className="ml-1 text-xl " /></button>
         </div>
         
             {/* To-Do List */}
 
             <div>
-                <TodoItems text={"Well Hello There"} />
-                <TodoItems text={"Eat 4 Eggs"}/>
+                {todoList.map((item, index) =>{
+                    return <TodoItems key={index} text={item.text} />
+                })}
+                {/* <TodoItems text={"Well Hello There"} />
+                <TodoItems text={"Eat 4 Eggs"}/> */}
             </div>
 
     </div>
